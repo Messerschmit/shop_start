2450bae8d0bbe1d2d2bb11c4a28a55a04186718d
cats_n_rats <kasatkin87@gmail.com>
2017-10-07 22:16:48 +0300

    Поднял mvc модель

---
+++ components/Router.php
@@ -0,0 +1,62 @@
+<?php
+
+//require_once (ROOT.'/controllers/SiteController.php');
+
+class Router
+{
+    private $routes;
+
+    public function __construct()
+    {
+        $routesPath = ROOT.'/config/routes.php';
+        $this->routes = include ($routesPath);
+    }
+
+    /*
+     * Return request string
+     * @return string
+     *
+     * */
+    private function getURI(){
+
+        if(!empty($_SERVER['REQUEST_URI'])){
+
+            return trim($_SERVER['REQUEST_URI'], '/');
+        }
+    }
+
+    /*
+     * Return Controller and Action
+     * @return bool
+     * */
+    public function run(){
+
+        $uri = $this->getURI();
+
+        foreach ($this->routes as $uriPattern=>$path){
+
+            if (preg_match("~$uriPattern~", $uri)){
+                $internalRoute = preg_replace("~$uriPattern~", $path, $uri);
+                $segment = explode('/', $internalRoute);
+                $controllerName = ucfirst(array_shift($segment).'Controller');
+                $actionName = 'action'.ucfirst(array_shift($segment));
+                $parameters = $segment;
+
+                $controllerFile = ROOT.'/controllers/'.$controllerName.'.php';
+
+                if(file_exists($controllerFile)){
+                    include_once ($controllerFile);
+                }
+
+                $controllerObject = new $controllerName;
+
+                $result = call_user_func_array(array($controllerObject, $actionName), $parameters);
+
+                if($result != null){
+                    break;
+                }
+            }
+
+        }
+    }
+}
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+//FRONT CONTROLLER
+
+//1. Общие настройки
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+define('ROOT', dirname('__FILE__'));
+
+//2. Подключение файлов
+require_once (ROOT.'/components/Router.php');
+
+
+$router = new Router();
+
+$router->run();
